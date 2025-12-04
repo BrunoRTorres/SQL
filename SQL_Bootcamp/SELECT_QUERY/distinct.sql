bd2a6c4cdf033ae17ec78dce848c6fe51488800a
Bruno Ribas Torres <bruno@brunoribas.com>
2025-12-04 14:26:23 -0300

    Add examples for DISTINCT in SQL queries
    Introduces a new SQL file demonstrating the use of the DISTINCT keyword to remove duplicate values from query results, with sample queries and expected output.

---
+++ SQL_Bootcamp/SELECT_QUERY/distinct.sql
@@ -0,0 +1,16 @@
+DISTINCT --? Removes duplicates (repeated values), each value appears only once
+
+SELECT DISTINCT
+    country
+FROM table;
+
+-- Return unique list of all countries
+
+SELECT DISTINCT
+	COUNTRY
+FROM customers;
+
+COUNTRY
+Germany
+UK
+USA
