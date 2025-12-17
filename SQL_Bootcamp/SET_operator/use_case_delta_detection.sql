600de8e50f8d3e31185ac9a5e57392c26b27284e
Bruno Ribas Torres <bruno@brunoribas.com>
2025-12-17 20:13:47 -0300

    Add delta detection SQL use case and related images
    Added a SQL script describing delta detection and data completeness check using SET operators. Included supporting images for data completeness check and delta detection in the SET_operator directory.

---
+++ SQL_Bootcamp/SET_operator/use_case_delta_detection.sql
@@ -0,0 +1,4 @@
+DELTA DETECTION --? Identifying the differences or changes (delta) between two batches of data
+
+DATA COMPLETENESS CHECK --? EXCEPT operator can be used to compare tables to detect discrepancies between databases
+
