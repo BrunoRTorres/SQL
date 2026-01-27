5af5e6d41afd6f39f32a90f5788142668d03e3bc
Bruno Ribas Torres <bruno@brunoribas.com>
2026-01-27 14:07:58 -0300

    Rename MIN/MAX.sql and add running & rolling total notes
    Renamed MIN/MAX.sql to MAX&MIN.sql for consistency. Added a new SQL file with notes explaining running and rolling total concepts under Aggregate/Use_case.

---
+++ SQL_Bootcamp/Window_Function/Aggregate/Use_case/Running&rolling_total.sql
@@ -0,0 +1,9 @@
+--? They aggregate sequence of members, and the aggregation is updated each time
+--? a new member is added
+
+--* Running total
+--? Aggregate all values from the beginning up to the point without dropping off older data
+
+--* Rolling total
+--? Aggregate all values within a fixed time window
+--? As new data is added, the oldest data point will be dropped
