aefa3bb1d2dd361e2d6b6df0dce41c24e70c28b9
Bruno Ribas Torres <bruno@brunoribas.com>
2026-05-06 14:20:54 -0300

    Add introduction to TEMP tables
    Add a new SQL file that documents temporary tables: they store intermediate results within the database session and are dropped when the session ends. Path: SQL_Bootcamp/CTAS&TEMP_tables/TEMP/introduction_TEMP_tables.sql.

---
+++ SQL_Bootcamp/CTAS&TEMP_tables/TEMP/introduction_TEMP_tables.sql
@@ -0,0 +1,4 @@
+Temporary tables --? Stores intermediate results in temporary storage
+                 --? within the database during the session.
+                 --? The database will drop all temporary tables once
+                 --? the session ends.
